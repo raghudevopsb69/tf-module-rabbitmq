477275946a0a1a20c9378d3802e606f67568d65d
Raghu K <raghuk.vit@gmail.com>
2023-01-06 08:09:42 +0530

    Update tf-modules/tf-module-rabbitmq/

--- rabbitmq.tf
+++ rabbitmq.tf
@@ -1,6 +1,8 @@
 resource "aws_spot_instance_request" "rabbitmq" {
-  ami           = data.aws_ami.centos8.id
-  instance_type = var.instance_type
+  ami                    = data.aws_ami.centos8.id
+  instance_type          = var.instance_type
+  subnet_id              = local.app_subnets_ids[0]
+  vpc_security_group_ids = [aws_security_group.allow_rabbitmq.id]
 
   tags = {
     Name = "${var.env}-rabbitmq"
