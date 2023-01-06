344bea4bce675b9bea93aa384e54f019de5c474e
Raghu K <raghuk.vit@gmail.com>
2023-01-06 08:05:13 +0530

    Update tf-modules/tf-module-rabbitmq/

--- rabbitmq.tf
+++ rabbitmq.tf
@@ -12,3 +12,9 @@ labauto ansible
 ansible-pull -i localhost, -U https://github.com/raghudevopsb69/roboshop-ansible roboshop.yml -e ROLE_NAME=rabbitmq -e ENV=${var.env}
 EOF
 }
+
+resource "aws_ec2_tag" "name-tag" {
+  resource_id = aws_spot_instance_request.rabbitmq.spot_instance_id
+  key         = "Name"
+  value       = "${var.env}-rabbitmq"
+}
