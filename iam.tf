08d87b5f5ab63f235d6a36dda4522ce11b134d47
Raghu K <raghuk.vit@gmail.com>
2023-01-13 09:04:06 +0530

    Update tf-modules/tf-module-rabbitmq/

---
+++ iam.tf
@@ -0,0 +1,64 @@
+resource "aws_iam_policy" "ssm-policy" {
+  name        = "${var.env}-ssm-to-ec2-rabbitmq"
+  path        = "/"
+  description = "${var.env}-ssm-to-ec2-rabbitmq"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : [
+          "ssm:GetParameterHistory",
+          "ssm:GetParametersByPath",
+          "ssm:GetParameters",
+          "ssm:GetParameter"
+        ],
+        "Resource" : [
+          "arn:aws:ssm:us-east-1:739561048503:parameter/param.dev*",
+          "arn:aws:ssm:us-east-1:739561048503:parameter/param.NEXUS*"
+        ]
+      },
+      {
+        "Sid" : "VisualEditor1",
+        "Effect" : "Allow",
+        "Action" : "ssm:DescribeParameters",
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "ec2-role" {
+  name = "${var.env}-ec2-rabbitmq"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "${var.env}-ec2-rabbitmq"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ssm-role-attach" {
+  role       = aws_iam_role.ec2-role.name
+  policy_arn = aws_iam_policy.ssm-policy.arn
+}
+
+resource "aws_iam_instance_profile" "ec2-instance-profile" {
+  name = "${var.env}-ec2-rabbitmq"
+  role = aws_iam_role.ec2-role.name
+}
+
--- rabbitmq.tf
+++ rabbitmq.tf
@@ -4,6 +4,7 @@ resource "aws_spot_instance_request" "rabbitmq" {
   subnet_id              = local.db_subnets_ids[0]
   vpc_security_group_ids = [aws_security_group.allow_rabbitmq.id]
   wait_for_fulfillment   = true
+  iam_instance_profile   = aws_iam_instance_profile.ec2-instance-profile.name
 
   tags = {
     Name = "${var.env}-rabbitmq"
