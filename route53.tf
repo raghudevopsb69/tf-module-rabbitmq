337e238d382cfad099d90ee80e71e8b1bbd6142c
Raghu K <raghuk.vit@gmail.com>
2023-01-13 08:50:56 +0530

    Update tf-modules/tf-module-rabbitmq/

--- route53.tf
+++ route53.tf
@@ -1,8 +1,8 @@
 resource "aws_route53_record" "component" {
   zone_id = data.aws_route53_zone.main.zone_id
-  name    = "${var.env}-${var.component}.${var.domain}"
-  type    = "CNAME"
+  name    = "${var.env}-rabbitmq.${var.domain}"
+  type    = "A"
   ttl     = 30
-  records = [local.dns_name]
+  records = [aws_spot_instance_request.rabbitmq.private_ip]
 }
 
